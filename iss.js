0468da2bd92f76a6e9528ab792ef7a4c11ef6fb1
Jea Oh Lee <jeaohlee99@gmail.com>
2022-09-29 21:34:39 -0700

    added the fetchcoordsbyip function

--- iss.js
+++ iss.js
@@ -1,6 +1,6 @@
 const request = require('request');
 
-const fetchMyIP = function(callback) {
+const fetchMyIP = (callback) => {
   request(`https://api.ipify.org?format=json`, (error, response, body) => {
     // console.log('error:', error);
     // console.log('response:',response);
@@ -10,12 +10,38 @@ const fetchMyIP = function(callback) {
       callback(error,null); return;
     }
     if (response.statusCode !== 200) {
-      const errorMessage = `statusCode ${response.statusCode} while fetching.`;
-      callback(errorMessage,null); return;
+      const errMsg = `statusCode ${response.statusCode} while fetching.`;
+      callback(Error(errMsg),null); return;
     }
-    const ip = JSON.parse(body);
+    const ip = JSON.parse(body).ip;
     callback(null,ip);
   });
 };
 
-module.exports = {fetchMyIP};
+const fetchCoordsByIP = (ip,callback) => {
+  //console.log(ip);
+  request(`https://ipwho.is/${ip}`, (error, response, body) => {
+    // console.log('error:', error);
+    // console.log('response:',response);
+    //console.log('body',body);
+    if (error) {
+      callback(error,null); return;
+    }
+
+    const jsonPrasedBody = JSON.parse(body);
+    if (!jsonPrasedBody.success) {
+      const errMsg = `success status was ${jsonPrasedBody.success}.\n
+      Server message: ${jsonPrasedBody.message}.\n
+      when fetching for IP ${jsonPrasedBody.ip}`;
+      callback(Error(errMsg),null);
+      return;
+    }
+    let data = JSON.parse(body);
+    console.log(data);
+    let {latitude, longitude} = data;
+    callback(error, longitude, latitude);
+  });
+};
+
+
+module.exports = {fetchMyIP,fetchCoordsByIP};
